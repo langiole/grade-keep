eac70f8383ec66fabeb4caa1b866ae32f7c88d74
jordanbarkley <jordan@charloe.com>
2018-04-25 18:39:47 -0400

    authetication test = passing

--- js/app.js
+++ js/app.js
@@ -11,15 +11,21 @@ var app = express();
 
 // README - init cloud-firestore
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./grade-keep-firebase-adminsdk-oovcw-4be84ecee7.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://grade-keep.firebaseio.com"
 });
-
 var db = admin.firestore();
+
+admin.auth().getUserByEmail("jordan@charloe.com")
+  .then(function(userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log("Successfully fetched user data:\n", userRecord.toJSON());
+  })
+  .catch(function(error) {
+    console.log("Error fetching user data:", error);
+  });
 
 var userRef = db.doc("/users/Uxr8lZrqdT8CAGvaYnkE/courses/OKeP4eXxQwAhtoi0cOZL")
 
